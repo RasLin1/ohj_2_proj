d8a4ee71c7a83acb2cdb32749bccc77294aa3867
Lumikuutti <227412132+lumikuutti@users.noreply.github.com>
2025-12-02 14:26:03 +0200

    project update

--- web_pages/main_menu/main_menu.js
+++ web_pages/main_menu/main_menu.js
@@ -1,1 +1,9 @@
 'use strict'
+
+const new_game = document.getElementById('new_game')
+const highscores = document.getElementById('highscores')
+const name_input = document.getElementById('name_input')
+
+new_game.addEventListener('click', function(){
+  name_input.showModal()
+})

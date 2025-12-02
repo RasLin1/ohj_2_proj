317b7c34832fea4cbf8e886bec44b011e7da7e8a
Lumikuutti <227412132+lumikuutti@users.noreply.github.com>
2025-12-02 15:08:25 +0200

    project update

--- web_pages/main_menu/main_menu.js
+++ web_pages/main_menu/main_menu.js
@@ -3,7 +3,29 @@
 const new_game = document.getElementById('new_game')
 const highscores = document.getElementById('highscores')
 const name_input = document.getElementById('name_input')
+const x = document.querySelector('span')
+const name_submit = document.getElementById('name_submit')
 
 new_game.addEventListener('click', function(){
   name_input.showModal()
+})
+
+highscores.addEventListener('click', function(){
+  window.location.href = "../highscores/highscores.html";
+})
+
+name_submit.addEventListener('click', function(){
+  const player_name = document.getElementById('player_name').value
+  //add so it grabs and assigns the given player as the value over to python
+  let start = confirm(`Start a new game as ${player_name}?`)
+  if (start === false) {
+    name_input.close()
+  }
+  else {
+    window.location.href = "../game/game.html"
+  }
+})
+
+x.addEventListener('click', function(){
+  name_input.close()
 })
